fdbd06cb35c6fb36af26ea36a491ff7a92c64b0b
Albin Matson Gyllang <albingyllang@gmail.com>
2021-02-18 16:35:44 +0100

    diagram with x-axis and y-axis, drawing a line with the data points

--- pop-flo/src/App.js
+++ pop-flo/src/App.js
@@ -1,20 +1,60 @@
 import React, { useState, useRef, useEffect } from 'react';
 import "./App.css"
-import { select } from "d3";
+import { select, line, curveCardinal, axisBottom,axisRight,scaleLinear } from "d3";
 
 function App() {
-  const [data, setData] = useState([25, 30, 45, 60, 20])
+  const [data, setData] = useState([100,120,75,99])
 
   const svgRef = useRef();
 
+
+
   useEffect(() => {
+    const svg = select(svgRef.current);
+    console.log("svg", svg);
+
     console.log("data", data)
 
-    const svg = select(svgRef.current);
-    console.log("svg", svg)
-    svg.selectAll("circle").data(data).join("circle").attr("r", value => value).attr("cx", value => value * 2).attr("cy", value => value * 2).attr("stroke", "blue");
     
+
+    //drawing the x-axis
+    const xScale = scaleLinear().domain([0,data.length-1]).range([0,300]);
+  
+    const xAxis = axisBottom(xScale).ticks(data.length).tickFormat(index => index + 1);
+
+    svg.select(".x-axis").style("transform","translateY(150px)").call(xAxis);
+
+
+    //drawing the y-axis
+    const yScale = scaleLinear().domain([0, 20 + Math.max(...data)]).range([150,0]);
+
+    const yAxis= axisRight(yScale);
+
+    svg.select(".y-axis").style("transform","translateX(300px)").call(yAxis);
+
+    
+    const myLine = line()
+      .x((value, index) => xScale(index))
+      .y(yScale)
+      .curve(curveCardinal);
+
+
+    svg
+      .selectAll(".line")
+      .data([data])
+      .join("path").attr("class", "line")
+      .attr("d", myLine)
+      .attr("fill", "none")
+      .attr("stroke", "blue");
+
+    //svg.selectAll("circle").data(data).join("circle").attr("r", value => value).attr("cx", value => value * 2).attr("cy", value => value * 2).attr("stroke", "blue");
+
+
+
     //D3 syntax! (above is a more concise way of coding)
+    //
+    //https://www.youtube.com/watch?v=9uEmNgHzPhQ&list=PLDZ4p-ENjbiPo4WH7KdHjh_EMI7Ic8b2B
+    //
     //"enter"-event is for handling first time rendered items.
     //"update"-event is used when data for an object has been changed, and what's suppose to happen with it next.
     //"exit"-event is what to do with the object when it has been removed
@@ -31,19 +71,27 @@ function App() {
     //  exit => exit.remove());
     //
     //
- 
 
- 
+
+
 
   }, [data]);
 
 
   return (
     <div className="App">
-      <svg ref={svgRef}></svg>
-      <button onClick={() => setData(data.map(value => value + 5))}>update data</button>
+      <svg ref={svgRef}>
+        <g className="x-axis"/>
+        <g className="y-axis"/>
 
+      </svg>
+      <br></br>
+      <br></br>
+      <br></br>
+      <button onClick={() => setData(data.map(value => value + 5))}>update data</button>
       <button onClick={() => setData(data.filter(value => value < 35))}>filter data</button>
+      <button onClick={() => setData(data => [...data, 20 * data.length])}>add data</button>
+
     </div>
   );
 }
