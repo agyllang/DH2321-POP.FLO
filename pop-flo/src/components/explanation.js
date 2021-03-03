dc3408fddec07778bfe4a88669ee45bd8ec6e9f2
Amalia <aberglof@kth.se>
2021-03-03 14:07:20 +0100

    structure changed

--- pop-flo/src/components/explanation.js
+++ pop-flo/src/components/explanation.js
@@ -14,15 +14,16 @@ class Explanation extends React.Component {
                 <p className="close" onClick={() => this.setState({show:!this.state.show})}>{this.state.show == true ? 'X':'Click to show'}</p>
                 {this.state.show == true ? (
                     <div>
-                                   <p>Hi and welcome to POP.FLO!</p>
-                                   <p>POP.FLO is a tool that gives you the possibility to
-                                       visualize moving patterns within Sweden, between Swedish counties, "län".
-                                   </p>
-                                   <p>Sweden has 21 counties - which shouldn't be mixed up
-                                       with the Swedish provinces, "landskap", of which there are 25. The counties have political
-                                       and admininistrative powers, the provinces have not. 
-                                   </p>
-                                   </div>
+                        <p>Hi and welcome to POP.FLO!</p>
+                        <p>POP.FLO is a tool that gives you the possibility to
+                            visualize moving patterns between Swedish counties, "län".
+                        </p>
+                        <p>Sweden has 21 counties - which shouldn't be mixed up
+                            with the Swedish provinces, "landskap", of which there are 25. The counties have political
+                            and admininistrative powers, the provinces have not. 
+                        </p>
+                        <p>You can see all of the 21 counties in the map below! Explore away!!</p>
+                    </div>
                 ): ''}
             </div>
 
