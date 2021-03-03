d781328d4a9d7c67c797513737a086bbe31d08ab
Amalia <aberglof@kth.se>
2021-03-03 13:58:26 +0100

    interaction added

--- pop-flo/src/components/explanation.js
+++ pop-flo/src/components/explanation.js
@@ -3,19 +3,27 @@ import React, { useState, useEffect } from "react";
 class Explanation extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            show: true,
+        }
     }
 
     render() {
         return(
-            <div>
-                <p>Hi and welcome to POP.FLO!</p>
-                <p>POP.FLO is a tool that gives you the possibility to
-                    visualize moving patterns within Sweden, between Swedish counties, "län".
-                </p>
-                <p>Sweden has 21 counties - which shouldn't be mixed up
-                    with the Swedish provinces, "landskap", of which there are 25. The counties have political
-                    and admininistrative powers, the provinces have not. 
-                </p>
+            <div className="explanation">
+                <p className="close" onClick={() => this.setState({show:!this.state.show})}>{this.state.show == true ? 'X':'Click to show'}</p>
+                {this.state.show == true ? (
+                    <div>
+                                   <p>Hi and welcome to POP.FLO!</p>
+                                   <p>POP.FLO is a tool that gives you the possibility to
+                                       visualize moving patterns within Sweden, between Swedish counties, "län".
+                                   </p>
+                                   <p>Sweden has 21 counties - which shouldn't be mixed up
+                                       with the Swedish provinces, "landskap", of which there are 25. The counties have political
+                                       and admininistrative powers, the provinces have not. 
+                                   </p>
+                                   </div>
+                ): ''}
             </div>
 
         )
