dc33c3e1922361616b7fc8b3e75c9a62d7f33e0c
Moa Engquist <moa.engquist@gmail.com>
2021-03-04 13:31:35 +0100

    Sankey

--- pop-flo/src/App.js
+++ pop-flo/src/App.js
@@ -1,13 +1,21 @@
 import React, { useState, useEffect } from "react"
+//import * as d3 from "d3";
+
 import WorldMap from "./components/WorldMap"
+import Sankey from "./components/SankeyContainer"
+
 import "./App.css"
+
 
 function App() {
 
   return (
     <div className="App">
+      <Sankey/>
       <WorldMap />
     </div>
+    
+    
   );
 }
 
