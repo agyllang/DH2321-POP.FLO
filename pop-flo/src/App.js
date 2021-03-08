27a165093dbf35985d19664a12ab6ea6de509525
Amalia <aberglof@kth.se>
2021-03-08 11:16:08 +0100

    routing added

--- pop-flo/src/App.js
+++ pop-flo/src/App.js
@@ -7,25 +7,36 @@ import "./App.css"
 import Explanation from "./components/explanation"
 import About from "./components/about";
 import GetData from "./components/GetData";
+import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom'
 
 
 function App() {
   const [selectedCounty, setSelectedCounty] = useState(null);
 
   return (
-    <div className="App">
-
-      <div>selected county:{selectedCounty}]</div>
-      <DropDown selected={selectedCounty} selectCounty={county => setSelectedCounty(county)} />
-      <MapContainer selected={selectedCounty} selectCounty={county => setSelectedCounty(county)}/>
-      <LinearScale />
-      <Explanation/>
-      <About/>
-      <Sankey/>
-    {/*   <GetData /> */}
-    </div>
-    
-    
+    <div>
+      <Router>
+      <div className="navbar">
+        <div className="navbuttons">
+        <Link to="/about"><button className="navbutton">About us & POP.FLO</button></Link>
+        </div>
+      </div>
+        <Switch>
+          <Route exact path="/" render={() => 
+          <div className="App">
+          <div>selected county:{selectedCounty}]</div>
+          <DropDown selected={selectedCounty} selectCounty={county => setSelectedCounty(county)} />
+          <MapContainer selected={selectedCounty} selectCounty={county => setSelectedCounty(county)}/>
+          <LinearScale />
+          <Sankey/>
+          </div>
+          
+          }></Route>
+          <Route path="/explanation" render={() => <Explanation/>}></Route>
+          <Route path="/about" render={() => <About/>}></Route>
+        </Switch>
+    </Router>   
+    </div> 
   );
 }
 
