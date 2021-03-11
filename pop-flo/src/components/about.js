9b2b0ca3f86fc3ffd131c08ab80e10f314098545
Albin Matson Gyllang <albingyllang@gmail.com>
2021-03-11 18:59:23 +0100

    design

--- pop-flo/src/components/about.js
+++ pop-flo/src/components/about.js
@@ -90,8 +90,7 @@ class About extends React.Component {
                                             {this.state.selectedPerson== "Amalia"? <b> Amalia Berglöf</b>:' Amalia Berglöf'}, 
                                             {this.state.selectedPerson== "Hilda"? <b> Hilda Robertsson</b>:' Hilda Robertsson'} and  
                                             {this.state.selectedPerson== "Moa"? <b> Moa Engquist</b>:' Moa Engquist'}.
-                                            We built this tool as our final project in our course in Information Visualization <a href="https://www.kth.se/student/kurser/kurs/DH2321">(DH2321)</a>
-                                             at KTH.</p>
+                                            We built this tool as our final project in our course in Information Visualization <a href="https://www.kth.se/student/kurser/kurs/DH2321">DH2321</a> at KTH.</p>
                                         <p>Our goal is to visualize movement in a simple, yet interesting way! We want to
                                             keep the simpleness and readability of a map, while adding visualizations to
                                             help interpret the data. It's up to you if we succeeded of course. 
